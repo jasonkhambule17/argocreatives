136dce6503faa43a527419718b17e7026d0a8726
jasonkhambule17 <jasonkhambule@gmail.com>
2020-06-15 10:06:34 +0200

    images

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -7,7 +7,24 @@ ActiveAdmin.register Article do
   #
    permit_params :title, :text, :author, :preview_text, :article_image
 
-   
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.input :article_image, as: :file
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :text
+      row :preview_text
+      row :author
+      row :article_image do |image|
+        image_tag url_for(image.article_image)
+      end
+    end
+  end
 
   #
   # or
--- db/migrate/20200510163601_create_articles.rb
+++ db/migrate/20200510163601_create_articles.rb
@@ -4,7 +4,6 @@ class CreateArticles < ActiveRecord::Migration[6.0]
       t.string :title
       t.text :text
       t.string :author
-      t.binary :article_image
       t.string :preview_text
       t.timestamps
     end
